83636e57f935e25a64c23b87f90cfaf1bce4f269
David Cabrero <david.cabrero@madsgroup.org>
2020-10-07 11:59:14 +0200

    Add 3.1: SMTP code

--- send_emails.py
+++ send_emails.py
@@ -3,6 +3,8 @@
 import sys
 from pathlib import Path
 import csv
+import smtplib
+import configparser
 
 if len(sys.argv) != 2:
     print(f"Usage: {Path(sys.argv[0]).name} file.csv")
@@ -14,4 +16,19 @@ with open(filename, newline= '') as f:
     reader = csv.reader(f, skipinitialspace= True, quotechar= '"')
     data = { email: student_data for email, *student_data in reader }
 
-print(data)
+config = configparser.ConfigParser()
+env = Path(__file__).parent / ".env"
+config.read(env)
+username = config['DEFAULT']['username']
+password = config['DEFAULT']['password']
+from_email = config['DEFAULT']['from'] 
+
+to_email = "test@gmail.com"
+
+server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+server.login(username, password)
+server.sendmail(
+    from_email, 
+    to_email,
+  "this message is from python")
+server.quit()
